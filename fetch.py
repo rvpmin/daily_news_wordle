2d220662cc57427a905573940d0775a4c02dcf54
rvpmin <roxanaperezmedina6@gmail.com>
2025-02-21 09:54:23 -0600

    updated fetch file

---
+++ fetch.py
@@ -0,0 +1,21 @@
+import requests
+import os
+
+api_key = os.getenv("NYT_API_KEY")
+if not api_key:
+    raise ValueError("NYT_API_KEY environment variable is not set.")
+
+url = f"https://api.nytimes.com/svc/mostpopular/v2/viewed/1.json"
+params={
+    "api-key": api_key
+}
+
+response = requests.get(url,params=params)
+
+if response.status_code == 200:
+    data = response.json
+    print("Datos recibidos:")
+    print(data)
+else:
+    print(f'Error: {response.status_code}')
+    print(response.text)

6286abe57d259257afa9b228326c795c89a29a68
rvpmin <roxanaperezmedina6@gmail.com>
2025-04-01 12:53:47 -0600

    SCRUM-11 changed api used

--- fetch.py
+++ fetch.py
@@ -2,22 +2,28 @@ import requests
 import os
 import json
 
-api_key = os.getenv("NYT_API_KEY")
+api_key = os.getenv("NYT_API")
 if not api_key:
-    raise ValueError("NYT_API_KEY environment variable is not set.")
+    raise ValueError("NYT_API environment variable is not set.")
 
-url = f"https://api.nytimes.com/svc/mostpopular/v2/viewed/1.json"
-params={
-    "api-key": api_key
-}
+url = f"https://api.nytimes.com/svc/topstories/v2/world.json?api-key={api_key}"
 
-response = requests.get(url,params=params)
 
-if response.status_code == 200:
+try:
+    response = requests.get(url)
     data = response.json()
-    print("Datos recibidos:")
-    print(data)
-else:
-    print(f'Error: {response.status_code}')
-    print(response.text)
+    response.raise_for_status()
 
+
+    with open('nyt_data.json', 'w', encoding='utf-8') as f:
+        json.dump(data,f,ensure_ascii=False, indent=4)
+
+    print(f"Total de artículos recibidos: {len(data.get('results', []))}")
+
+except requests.exceptions.RequestException as e:
+    print(f"Error en la solicitud: {e}")
+except json.JSONDecodeError:
+    print("Error al decodificar la respuesta JSON")
+except Exception as e:
+    print(f"Error inesperado: {e}")
+#print("https://api.nytimes.com/svc/mostpopular/v2/viewed/1.json?api-key=uAoMAen9WjPa9A0GXsDI39V5u92XdT59"== response.request.url)

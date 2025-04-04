aa058d7b7f33acba938a05605bba61da5c0e435c
rvpmin <roxanaperezmedina6@gmail.com>
2025-04-04 12:57:40 -0600

    first docker compose

--- www/settings/production.py
+++ www/settings/production.py
@@ -1,6 +1,23 @@
 from .base import *
+import environ
 
 DEBUG = False
+
+#Inicializa environ
+env = environ.Env()
+environ.Env.read_env() #Lee el archivo .env
+
+# Configuración de la base de datos                                            
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+    'NAME': env('POSTGRES_DB'),
+        'USER': env('POSTGRES_USER'),
+        'PASSWORD': env('POSTGRES_PASSWORD'),
+        'HOST': 'db',  # Nombre del servicio en docker-compose                  
+    'PORT': '5432',
+    }
+}
 
 try:
     from .local import *
